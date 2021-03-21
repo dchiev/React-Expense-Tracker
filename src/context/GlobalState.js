58e7641a735ec4e0bac6f2dfce2264c07f1d2951
Dan Chiev <danchiev2@Gmail.com>
2021-03-21 16:43:14 -0400

    added add functionality

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -3,12 +3,7 @@ import AppReducer from "./AppReducer";
 
 // Initial State
 const initialState = {
-  transactions: [
-    { id: 1, text: "Flower", amount: -20 },
-    { id: 2, text: "Salary", amount: 300 },
-    { id: 3, text: "Book", amount: -10 },
-    { id: 4, text: "Camera", amount: 150 },
-  ],
+  transactions: [],
 };
 
 // Create Contexts
@@ -23,10 +18,17 @@ export const GlobalProvider = ({ children }) => {
   function deleteTransaction(id) {
     dispatch({ type: "DELETE_TRANSACTION", payload: id });
   }
+  function addTransaction(transaction) {
+    dispatch({ type: "ADD_TRANSACTION", payload: transaction });
+  }
 
   return (
     <GlobalContext.Provider
-      value={{ transactions: state.transactions, deleteTransaction }}
+      value={{
+        transactions: state.transactions,
+        deleteTransaction,
+        addTransaction,
+      }}
     >
       {children}
     </GlobalContext.Provider>
